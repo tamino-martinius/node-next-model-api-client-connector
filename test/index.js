b8ce894e8cce434533b568f4a9a782ddf3391c73
tamino-martinius <dev@zaku.eu>
2017-03-06 19:12:41 +0100

    Updated specs to work with api router

--- test/index.js
+++ test/index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 const NextModelApiClientConnector = require('..');
+const NextModelApiRouter = require('../../next-model-api-router');
 const NextModel = require('next-model');
+
 const expect = require('expect.js');
 const expectChange = require('expect-change');
 const sinon = require('sinon');
+
 const request = require('then-request');
 const pluralize = require('pluralize');
 const URL = require('url');
@@ -14,93 +17,86 @@ const lodash = require('lodash');
 const includes = lodash.includes;
 const words = lodash.words;
 
-def('BaseModel', () => class BaseModel extends NextModel {
-  static get connector() {
-    return NextModelApiClientConnector;
-  }
-});
-
-def('User', () => class User extends $BaseModel {
-  static get modelName() { return 'User'; }
-
-  static get schema() {
-    return {
-      id: { type: 'integer' },
-      name: { type: 'string' },
-      age: { type: 'integer' },
-    };
-  }
-
-  static get tableName() { return $tableName;}
-
-  static get defaultScope() { return $defaultScope; }
-  static get defaultOrder() { return $defaultOrder; }
-
-  static get _skip() { return $skip; }
-  static get _limit() { return $limit; }
-
-  static get routeDomain() { return $routeDomain; }
-  static get routeIsRelative() { return $routeIsRelative; }
-  static get routePath() { return $routePath; }
-  static get routeVersion() { return $routeVersion; }
-  static get routePostfix() { return $routePostfix; }
-
-  static get allActionPath() { return $allActionPath; }
-  static get allActionMethod() { return $allActionMethod; }
-  static get firstActionPath() { return $firstActionPath; }
-  static get firstActionMethod() { return $firstActionMethod; }
-  static get lastActionPath() { return $lastActionPath; }
-  static get lastActionMethod() { return $lastActionMethod; }
-  static get countActionPath() { return $countActionPath; }
-  static get countActionMethod() { return $countActionMethod; }
-  static get insertActionPath() { return $insertActionPath; }
-  static get insertActionMethod() { return $insertActionMethod; }
-  static get updateActionPath() { return $updateActionPath; }
-  static get updateActionMethod() { return $updateActionMethod; }
-  static get deleteActionPath() { return $deleteActionPath; }
-  static get deleteActionMethod() { return $deleteActionMethod; }
-});
-
-def('tableName', () => 'users');
-
-def('defaultScope', () => undefined);
-def('defaultOrder', () => undefined);
-
-def('skip', () => undefined);
-def('limit', () => undefined);
-
-def('routeDomain', () => undefined);
-def('routeIsRelative', () => undefined);
-def('routePath', () => undefined);
-def('routeVersion', () => undefined);
-def('routePostfix', () => undefined);
-
-def('allActionPath', () => undefined);
-def('allActionMethod', () => undefined);
-def('firstActionPath', () => undefined);
-def('firstActionMethod', () => undefined);
-def('lastActionPath', () => undefined);
-def('lastActionMethod', () => undefined);
-def('countActionPath', () => undefined);
-def('countActionMethod', () => undefined);
-def('insertActionPath', () => undefined);
-def('insertActionMethod', () => undefined);
-def('updateActionPath', () => undefined);
-def('updateActionMethod', () => undefined);
-def('deleteActionPath', () => undefined);
-def('deleteActionMethod', () => undefined);
-
 const requestStub = sinon.stub(request, '_request');
 
 describe('NextModelApiClientConnector', function() {
   this.timeout(10000);
+
+  def('Router', () => class Router extends NextModelApiRouter {
+    static get isClient() {
+      return true;
+    }
+  });
+
+  def('router', () => new $Router({
+    domain: $apiDomain,
+    path: $apiPath,
+    version: $apiVersion,
+  }));
+
+  def('apiDomain', () => undefined);
+  def('apiPath', () => undefined);
+  def('apiVersion', () => undefined);
+
+  def('connector', () => new NextModelApiClientConnector({ router: $router }));
+
+  def('BaseModel', () => class BaseModel extends NextModel {
+    static get connector() {
+      return $connector;
+    }
+  });
+
+  def('User', () => class User extends $BaseModel {
+    static get modelName() { return 'User'; }
+
+    static get schema() {
+      return {
+        id: { type: 'integer' },
+        name: { type: 'string' },
+        age: { type: 'integer' },
+      };
+    }
+
+    static get tableName() { return $tableName;}
+
+    static get defaultScope() { return $defaultScope; }
+    static get defaultOrder() { return $defaultOrder; }
+
+    static get _skip() { return $skip; }
+    static get _limit() { return $limit; }
+  });
+
+  def('tableName', () => 'users');
+
+  def('defaultScope', () => undefined);
+  def('defaultOrder', () => undefined);
+
+  def('skip', () => undefined);
+  def('limit', () => undefined);
+
+  beforeEach(function() {
+    $router.resource($User, {
+      defaults: $resourceDefaults,
+      only: $resourceOnly,
+      except: $resourceExcept,
+      collection: $resourceCollection,
+      member: $resourceMember,
+    });
+  });
+
+  def('resourceDefaults', () => undefined);
+  def('resourceOnly', () => undefined);
+  def('resourceExcept', () => undefined);
+  def('resourceCollection', () => undefined);
+  def('resourceMember', () => undefined);
+
   afterEach(function() {
     requestStub.reset();
   });
 
   const itMakesRequest = function(expectedMethod, expectedUrl) {
     it(`makes '${expectedMethod}' request to '${expectedUrl}'`, function() {
-      NextModelApiClientConnector[get('action')](get('param'));
+      $connector[get('action')](get('param'));
       expect(requestStub.calledOnce).to.be(true);
       const method = requestStub.firstCall.args[0];
       const urlObj = URL.parse(requestStub.firstCall.args[1]);
@@ -112,7 +108,7 @@ describe('NextModelApiClientConnector', function() {
 
     if (includes(['POST', 'PUT', 'PATCH'], expectedMethod)) {
       it(`has payload in body and empty querystring`, function() {
-        NextModelApiClientConnector[get('action')](get('param'));
+        $connector[get('action')](get('param'));
         const url = URL.parse(requestStub.firstCall.args[1]);
         const contentLength = requestStub.firstCall.args[2].headers['Content-Length'];
         expect(url.search).to.be(null);
@@ -120,7 +116,7 @@ describe('NextModelApiClientConnector', function() {
       });
     } else {
       it(`has payload in querystring and empty body`, function() {
-        NextModelApiClientConnector[get('action')](get('param'));
+        $connector[get('action')](get('param'));
         const url = URL.parse(requestStub.firstCall.args[1]);
         const contentLength = requestStub.firstCall.args[2].headers['Content-Length'];
         expect(url.search).to.not.be.empty();
@@ -143,7 +139,7 @@ describe('NextModelApiClientConnector', function() {
     }
 
     it(words(specTitle, /[^\n ]+/g).join(' '), function() {
-      NextModelApiClientConnector[get('action')](get('param'));
+      $connector[get('action')](get('param'));
       const urlObj = URL.parse(requestStub.firstCall.args[1]);
       const qs = querystring.parse(urlObj.query);
       const scope = JSON.parse(qs.scope);
@@ -166,100 +162,106 @@ describe('NextModelApiClientConnector', function() {
       itMakesRequest('POST', `/${path}`);
 
       context('when domain is present', function() {
-        def('routeDomain', () => 'http://example.com');
+        def('apiDomain', () => 'http://example.com');
 
         itMakesRequest('POST', `http://example.com/${path}`);
 
         context('when url starts with //', function() {
-          def('routeDomain', () => '//example.com');
+          def('apiDomain', () => '//example.com');
 
           itMakesRequest('POST', `//example.com/${path}`);
         });
 
         context('when url ends with /', function() {
-          def('routeDomain', () => 'http://example.com/');
-
-          itMakesRequest('POST', `http://example.com/${path}`);
-        });
-      });
-
-      context('when isRelative is present', function() {
-        def('routeIsRelative', () => true);
-
-        itMakesRequest('POST', `${path}`);
-
-        context('value is false', function() {
-          def('routeIsRelative', () => false);
-
-          itMakesRequest('POST', `/${path}`);
-        });
-
-        context('when domain is present', function() {
-          def('routeDomain', () => 'http://example.com');
+          def('apiDomain', () => 'http://example.com/');
 
           itMakesRequest('POST', `http://example.com/${path}`);
         });
       });
 
       context('when path is present', function() {
-        def('routePath', () => 'api');
+        def('apiPath', () => 'api');
 
         itMakesRequest('POST', `/api/${path}`);
 
         context('when path starts with /', function() {
-          def('routePath', () => '/api');
+          def('apiPath', () => '/api');
 
           itMakesRequest('POST', `/api/${path}`);
         });
 
         context('when path ends with /', function() {
-          def('routePath', () => 'api/');
+          def('apiPath', () => 'api/');
 
           itMakesRequest('POST', `/api/${path}`);
         });
 
         context('when version is present', function() {
-          def('routeVersion', () => 'v1');
+          def('apiVersion', () => 'v1');
 
           itMakesRequest('POST', `/api/v1/${path}`);
 
           context('when version starts with /', function() {
-            def('routeVersion', () => '/v1');
+            def('apiVersion', () => '/v1');
 
             itMakesRequest('POST', `/api/v1/${path}`);
           });
 
           context('when version ends with /', function() {
-            def('routeVersion', () => 'v1/');
+            def('apiVersion', () => 'v1/');
 
             itMakesRequest('POST', `/api/v1/${path}`);
           });
         });
       });
 
-      context('when postfix is present', function() {
-        def('routePostfix', () => '.json');
+      context('when resource defaults are present', function() {
+        def('resourceDefaults', () => ({
+          method: $method,
+          postfix: $postfix,
+        }));
+        def('method', () => undefined);
+        def('postfix', () => undefined);
 
-        itMakesRequest('POST', `/${path}.json`);
+        context('when postfix is present', function() {
+          def('postfix', () => '.json');
+
+          itMakesRequest('POST', `/${path}.json`);
+        });
+
+        context('when method is present', function() {
+          def('method', () => 'get');
+
+          itMakesRequest('GET', `/${path}`);
+        });
       });
     });
   };
 
   const itMakesActionModificationsFor = function(action, path, pathEnd) {
     describe('action requests', function() {
+      def('resourceOnly', () => ({
+        [action]: {
+          path: $path,
+          method: $method,
+        },
+      }));
+      def('path', () => undefined);
+      def('method', () => undefined);
+
       context('when action path present', function() {
-        def(action + 'ActionPath', () => 'foo');
+        def('path', () => 'foo');
 
         itMakesRequest('POST', `/${path}/foo`);
 
         context('when path starts with /', function() {
-          def(action + 'ActionPath', () => '/foo');
+          def('path', () => '/foo');
 
           itMakesRequest('POST', `/${path}/foo`);
         });
 
         context('when path ends with /', function() {
-          def(action + 'ActionPath', () => 'foo/');
+          def('path', () => 'foo/');
 
           itMakesRequest('POST', `/${path}/foo`);
         });
@@ -273,7 +275,7 @@ describe('NextModelApiClientConnector', function() {
         'PUT',
       ]) {
         context(`when action method '${method}' present`, function() {
-          def(action + 'ActionMethod', () => method);
+          def('method', () => method);
 
           itMakesRequest(method, `/${path + pathEnd}`);
         });
@@ -283,7 +285,7 @@ describe('NextModelApiClientConnector', function() {
 
   const itChecksCollectionPayload = function(action) {
     context('collection request payload', function() {
-      def(action + 'ActionMethod', () => 'GET');
+      def('resourceDefaults', () => ({ method: 'get' }));
 
       itChecksPayload({
         scope: {},
@@ -406,7 +408,7 @@ describe('NextModelApiClientConnector', function() {
 
   const itChecksInstancePayload = function(action) {
     context('instance request payload', function() {
-      def(action + 'ActionMethod', () => 'GET');
+      def('resourceDefaults', () => ({ method: 'get' }));
       def('name', () => 'foo');
 
       itChecksPayload({
@@ -485,10 +487,10 @@ describe('NextModelApiClientConnector', function() {
     context('when user is new', function() {
       def('param', () => $User.build({ name: $name }));
 
-      itMakesCollectionRequestsTo('/create').forAction('insert');
+      itMakesCollectionRequestsTo('/create').forAction('create');
 
       context('instance request payload', function() {
-        def('insertActionMethod', () => 'GET');
+        def('resourceDefaults', () => ({ method: 'get' }));
         def('name', () => 'foo');
 
         itChecksPayload({
@@ -520,7 +522,7 @@ describe('NextModelApiClientConnector', function() {
         return user;
       });
 
-      itMakesInstanceRequestsTo('').forAction('update');
+      itMakesInstanceRequestsTo('/update').forAction('update');
     });
   });
 
